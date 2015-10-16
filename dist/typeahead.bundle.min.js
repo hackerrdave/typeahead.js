5ededd6223fb67fa0afb113700d35a97b29c9222
Dave Kerr <davek09@gmail.com>
2015-10-16 13:39:13 -0400

    0.11.2

--- dist/typeahead.bundle.min.js
+++ dist/typeahead.bundle.min.js
@@ -1,5 +1,5 @@
 /*!
- * typeahead.js 0.11.1
+ * typeahead.js 0.11.2 For Haven Life
  * https://github.com/twitter/typeahead.js
  * Copyright 2013-2015 Twitter, Inc. and other contributors; Licensed MIT
  */
